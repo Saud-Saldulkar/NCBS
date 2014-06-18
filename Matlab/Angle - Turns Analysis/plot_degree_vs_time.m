c6dbf7f9dc05247959b1f5c1998c980685827105
Saud Saldulkar <saud28@hotmail.com>
2014-06-18 09:53:06 +0530

    v9

--- Matlab/Angle - Turns Analysis/plot_degree_vs_time.m
+++ Matlab/Angle - Turns Analysis/plot_degree_vs_time.m
@@ -1,4 +1,5 @@
 function [output] = plot_degree_vs_time(file_name, bin_size1, bin_size2)
+
 % Angle data extracted required with [theta] variable
 %
 % usage example:
@@ -7,16 +8,21 @@ function [output] = plot_degree_vs_time(file_name, bin_size1, bin_size2)
 % bin_size1 = 8;
 % bin_size2 = 25;
 % [output] = plot_degree_vs_time(file_name, bin_size1, bin_size2);
+
+% [output] = mean | STD DEV | STD ERROR
 % --------------------------------------------------------------------
 
     load(file_name);
     
-    theta = convert_rad_degrees(theta);
+    theta1 = convert_rad_degrees(theta);
        
-    theta = relative_turning(theta, bin_size1);
+    theta2 = relative_turning(theta1, bin_size1);
 
-    [output] = extract_mean(theta, bin_size2);
-
+    theta3 = extract_mean(theta2, bin_size2);
+    
+    theta4 = extract_std_error(theta3, bin_size2);
+    
+    output = theta4;
 end
 
 function [output] = convert_rad_degrees(input)
@@ -30,7 +36,7 @@ function [output] = relative_turning(theta, bin_size1)
     for i = 1:bin_size1:length(theta)
         
         start_frame = i;
-        end_frame = i + bin_size1;
+        end_frame = i + (bin_size1 - 1);
         
         if (end_frame > length(theta))
             return;
@@ -52,7 +58,7 @@ function [output] = extract_mean(theta, bin_size2)
     for i = 1:bin_size2:length(theta)
         
         start_frame = i;
-        end_frame = i + bin_size2;
+        end_frame = i + (bin_size2 - 1);
         
         if (end_frame > length(theta))
             return;
@@ -61,12 +67,17 @@ function [output] = extract_mean(theta, bin_size2)
         data_block = theta(start_frame:end_frame, 1);
         
         block_mean = mean(data_block);
+        block_std_dev = std(data_block);
         
         output(iterator, 1) = block_mean;
-        output(iterator, 2) = std(data_block);
+        output(iterator, 2) = block_std_dev;
         iterator = iterator + 1;
     end
 end
 
-
-
+function [output] = extract_std_error(theta, bin_size2)
+   for i = 1:length(theta(:,1))
+       theta(i,3) = (theta(i,2)/ sqrt(bin_size2));
+   end
+   output = theta;
+end
