a0143472845a5a81ccb62b1c958d3fd60f822731
Saud Saldulkar <saud28@hotmail.com>
2014-09-01 09:59:46 +0530

    v19

---
+++ Matlab/extract_theta.m
@@ -0,0 +1,25 @@
+% Extract THETA(angle data) from the raw data files
+clc;
+clear all;
+close all;
+
+no_files = 10;
+
+for i = 1:no_files
+    file_name = strcat('n', int2str(i), '.mat');
+    load(file_name);
+    
+    theta = angle;
+    
+    for i = 1:length(theta)
+        theta(i,1) = (theta(i,1) * 180) / pi;    
+        if (sign(theta(i,1)) == -1)
+            theta(i,1) = theta(i,1) + 360;
+        end
+    end
+    
+    save(file_name, 'theta');
+end
+
+clearvars;
+display('Done extracting THETA');
