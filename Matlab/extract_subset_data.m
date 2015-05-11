54228bea7ab9d2c6ef82ed83a9ff35d59e12b980
Saud Saldulkar <saud28@hotmail.com>
2015-05-11 09:08:58 +0530

    v40

--- Matlab/extract_subset_data.m
+++ Matlab/extract_subset_data.m
@@ -3,21 +3,26 @@ clc;
 clear all;
 close all;
 
-% -----------------------------------
-no_files = 10;
-start_frame = 1000;
-end_frame = 2000;
-% -----------------------------------
+try
+    % -----------------------------------
+    no_files = 10;
+    start_frame = 3000;
+    end_frame = 4000;
+    % -----------------------------------
 
-for i = 1:no_files
-   file_name = strcat('n', int2str(i), '.mat');
-   load(file_name);
-     
-   x_pos = x_pos(start_frame:end_frame,1);
-   y_pos = y_pos(start_frame:end_frame,1);
-   
-   save(file_name, 'x_pos', 'y_pos');
+    for i = 1:no_files
+       file_name = strcat('n', int2str(i), '.mat');
+       load(file_name);
+
+       x_pos = x_pos(start_frame:end_frame,1);
+       y_pos = y_pos(start_frame:end_frame,1);
+
+       save(file_name, 'x_pos', 'y_pos');
+    end
+
+    clearvars;
+    display('Done extracting subset data');
+    
+catch err
+   display('Error'); 
 end
-
-clearvars;
-display('Done extracting subset data');
